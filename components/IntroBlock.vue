6640b1f5cdab7054fc3719ab94ac172811ae11cc
Tiffany VACHEZ <tiffanyvachez@jeremys-macbook-pro.home>
2021-08-06 18:38:30 +0200

    feat: add intro block

---
+++ components/IntroBlock.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="intro-block">
+    <div class="intro-block__left">
+      <!-- <img
+        class="intro-block__image"
+        src="~/assets/img/intro.jpg"
+        alt="image intro"
+      /> -->
+    </div>
+    <div class="intro-block__right">
+      <h1 class="intro-block__title">Votre litige,<br />nos solutions</h1>
+      <p class="intro-block__text">
+        Une <span class="bold">démarche collective</span> pour une issue
+        commune.
+      </p>
+      <p class="intro-block__text">
+        Des solutions <span class="bold">amiables</span>, plus
+        <span class="bold">économiques</span> et plus
+        <span class="bold">rapides</span> existent pour résoudre un litige en
+        évitant un procès toujours long, aléatoire et coûteux.
+      </p>
+      <p class="intro-block__text">
+        Nous pouvons vous guider et vous assister, au travers de
+        <span class="bold">solutions juridiquement éprouvées</span>, pour mettre
+        en place une telle démarche.
+      </p>
+      <p class="intro-block__text">
+        <span class="bold">La condition : que tous le veuillent.</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.intro-block {
+  display: flex;
+}
+
+.intro-block__left {
+  width: 55%;
+  background-image: url('~/assets/img/intro.jpg');
+  background-size: cover;
+  background-position: center;
+}
+.intro-block__right {
+  width: 45%;
+  padding: 40px;
+  background-color: #e0eef6;
+}
+
+.intro-block__title {
+  font-family: 'Oswald', sans-serif;
+  text-transform: uppercase;
+  font-size: 74px;
+  line-height: 1.2;
+  margin-top: 0;
+}
+
+.intro-block__text {
+  font-size: 23px;
+  margin: 40px 0;
+}
+
+.bold {
+  font-weight: bold;
+  color: blue;
+}
+</style>
