8751ac34ba43c745f2ee1caa12420d5f4bf19916
Tiffany VACHEZ <tiffanyvachez@jeremys-macbook-pro.home>
2021-08-23 19:41:09 +0200

    chore: fix build

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,11 +45,13 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ['vue2-smooth-scroll'],
+  },
 
   // Github pages
   target: 'static',
   router: {
     base: '/arbitrex/',
   },
-}
+};
