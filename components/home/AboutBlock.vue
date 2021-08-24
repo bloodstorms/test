914f30973c17f303092b913ba808d65386ab2e86
Tiffany VACHEZ <tiffanyvachez@jeremys-macbook-pro.home>
2021-08-24 16:52:31 +0200

    feat(about-block): mobile version

--- components/home/AboutBlock.vue
+++ components/home/AboutBlock.vue
@@ -47,10 +47,21 @@ export default {
 
 <style scoped lang="scss">
 @import '~/assets/styles/colors';
+@import '~/assets/styles/mixins';
 
 .about-block {
   background-color: $blue-extralight;
   padding: 80px 10%;
+
+  @include phone {
+    padding: 80px 20px;
+  }
+  @include tablet-portrait {
+    padding: 80px 40px;
+  }
+  @include tablet-landscape {
+    padding: 80px 80px;
+  }
 }
 
 .about-block__title {
@@ -59,6 +70,10 @@ export default {
   line-height: 1.1;
   letter-spacing: 0;
   margin: 0;
+
+  @include phone {
+    font-size: 45px;
+  }
 }
 
 .about-block__subtitle {
@@ -66,18 +81,33 @@ export default {
   font-weight: 100;
   line-height: 1.3;
   margin: 50px 0;
+
+  @include phone {
+    font-size: 16px;
+    margin: 30px 0;
+  }
 }
 
 .about-block__accordions {
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
+
+  @include device-portrait {
+    flex-direction: column;
+  }
 }
 
 // Custom styles
 ::v-deep .accordion {
-  min-width: 300px;
+  min-width: 270px;
   max-width: 30%;
+
+  @include device-portrait {
+    margin-bottom: 40px;
+    max-width: 100%;
+    width: 100%;
+  }
 
   .collapsable-item {
     padding: 10px 20px 0 20px;
