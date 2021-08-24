ccb946092c7fa4c64350d55e6ba578b6a1e1ac40
Tiffany VACHEZ <tiffanyvachez@jeremys-macbook-pro.home>
2021-08-24 15:39:54 +0200

    feat(keywords-block): mobile version

--- components/home/KeywordsBlock.vue
+++ components/home/KeywordsBlock.vue
@@ -62,10 +62,21 @@ export default {
 
 <style scoped lang="scss">
 @import '~/assets/styles/colors';
+@import '~/assets/styles/mixins';
 
 .keywords-block {
   background-color: $grey-extralight;
   padding: 60px 0 150px 120px;
+
+  @include phone {
+    padding: 20px 0 80px 20px;
+  }
+  @include tablet-portrait {
+    padding: 40px 0 100px 40px;
+  }
+  @include tablet-landscape {
+    padding: 40px 0 100px 80px;
+  }
 }
 
 .keywords-block__word {
@@ -78,8 +89,31 @@ export default {
   margin-left: 120px;
   line-height: 1.5;
 
+  @include phone {
+    font-size: 40px;
+    margin-left: 40px;
+  }
+  @include tablet-portrait {
+    font-size: 80px;
+    margin-left: 60px;
+  }
+  @include tablet-landscape {
+    font-size: 100px;
+    margin-left: 80px;
+  }
+
   &:nth-child(2) {
     margin-left: 300px;
+
+    @include phone {
+      margin-left: 80px;
+    }
+    @include tablet-portrait {
+      margin-left: 150px;
+    }
+    @include tablet-landscape {
+      margin-left: 220px;
+    }
   }
 }
 
@@ -89,6 +123,22 @@ export default {
   left: -120px;
   top: -60px;
   color: $green;
+
+  @include phone {
+    font-size: 70px;
+    left: -40px;
+    top: -30px;
+  }
+  @include tablet-portrait {
+    font-size: 120px;
+    left: -60px;
+    top: -45px;
+  }
+  @include tablet-landscape {
+    font-size: 155px;
+    left: -80px;
+    top: -60px;
+  }
 }
 .keywords-block__plus {
   position: absolute;
@@ -96,6 +146,22 @@ export default {
   left: -140px;
   top: -70px;
   color: $blue-light;
+
+  @include phone {
+    font-size: 75px;
+    left: -40px;
+    top: -27px;
+  }
+  @include tablet-portrait {
+    font-size: 140px;
+    left: -70px;
+    top: -47px;
+  }
+  @include tablet-landscape {
+    font-size: 190px;
+    left: -100px;
+    top: -70px;
+  }
 }
 .keywords-block__amp {
   position: absolute;
@@ -103,11 +169,49 @@ export default {
   left: -130px;
   top: 0px;
   color: $blue;
+
+  @include phone {
+    font-size: 40px;
+    left: -35px;
+  }
+  @include tablet-portrait {
+    font-size: 80px;
+    left: -60px;
+  }
+  @include tablet-landscape {
+    font-size: 100px;
+    left: -80px;
+  }
+}
+
+.keywords-block__text {
+  font-size: 20px;
+
+  @include phone {
+    font-size: 16px;
+  }
+  @include tablet-portrait {
+    font-size: 20px;
+  }
+  @include tablet-landscape {
+    font-size: 20px;
+  }
 }
 
 .keywords-block__button-container {
   width: 400px;
-  margin-left: 170px;
+  margin-left: 130px;
   margin-top: 100px;
+
+  @include phone {
+    margin-left: 45px;
+    margin-top: 50px;
+  }
+  @include tablet-portrait {
+    margin-left: 70px;
+  }
+  @include tablet-landscape {
+    margin-left: 90px;
+  }
 }
 </style>
